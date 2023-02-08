38c46cc0361bd1de260ef591706959286c9a6ae9
Felix <felixyeu25@gmail.com>
2023-02-08 14:38:09 +0100

    Debut auth en Django surement à réadapter avec flutter + test

--- backend/backend/urls.py
+++ backend/backend/urls.py
@@ -15,7 +15,12 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from django.contrib import auth
+from django.contrib.auth.views import LoginView, LogoutView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('account/', include('django.contrib.auth.urls')),
+    path('login/', LoginView.as_view(), name='login'),
+    path('logout/', LogoutView.as_view(), name='logout'),
 ]
