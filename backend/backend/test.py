de13b985840b3536b925174aae9cf5f17bdce041
Felix <felixyeu25@gmail.com>
2023-02-10 09:07:09 +0100

    resize + tests penser à modifier path

--- backend/backend/test.py
+++ backend/backend/test.py
@@ -1,6 +1,7 @@
 from django.contrib.auth.models import User
 from django.test import RequestFactory, TestCase
-
+from PIL import Image
+import os
 
 
 class LoginTest(TestCase):
@@ -24,3 +25,20 @@ class LoginTest(TestCase):
         # test que l'utilisateur est bien connectée
         self.assertTrue(request.user.is_authenticated)
         self.assertEqual(request.user.username, 'username')
+
+class testresize(TestCase):
+    def test_resize_image(self):
+        im = Image.open("")# dossier test
+
+        self.assertNotEqual(im.size, (128, 128))
+        im_resized = im.resize((128, 128))
+        self.assertEqual(im_resized.size, (128, 128))
+    
+    def changeformattojpg(self):
+        im = Image.open("") #ouvrir l'image en PNG 
+
+        self.assertNotEqual(im.format, "JPEG")
+        im.save("", format="JPEG")
+
+        im_resaved = Image.open("") #ouvrir l'image en JPG 
+        self.assertEqual(im_resaved.format, "JPEG")
