9325b119c349ffba81ca302ce23c9cfef7e8c281
Jonh <jonathann.jouvin@free.fr>
2023-07-04 15:16:52 +0200

    ajout bdd mysql

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -80,6 +80,14 @@ WSGI_APPLICATION = 'backend.wsgi.application'
 
 DATABASES = {
     'default': {
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'valachercher',
+        'USER': 'jonh',
+        'PASSWORD': 'jonh',
+        'HOST': 'localhost',
+        'PORT': '3306',
+    },
+    'default2': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME':  BASE_DIR / 'db.sqlite3',
     }
